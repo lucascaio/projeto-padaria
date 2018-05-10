21304d621cb7026e8aedd86767e6f71f3abe40a9
Lucas Caio de Matos <matos.lucas@mitra.aqa.local>
2018-05-10 09:05:17 -0300

    Implementação do cadastro de fornada

--- app/src/main/java/com/example/matoslucas/padaria/repository/FornadaRepository.kt
+++ app/src/main/java/com/example/matoslucas/padaria/repository/FornadaRepository.kt
@@ -1,12 +1,12 @@
 package com.example.matoslucas.padaria.repository
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import com.example.matoslucas.padaria.constants.DataBaseConstants
 import com.example.matoslucas.padaria.entities.FornadaEntity
-import com.example.matoslucas.padaria.entities.UserEntity
 
-class FornadaRepository private constructor(context: Context){
+class FornadaRepository private constructor(context: Context) {
     private var mFPadariaDatabaseHelper: PadariaDatabaseHelper = PadariaDatabaseHelper(context)
 
     companion object {
@@ -19,45 +19,46 @@ class FornadaRepository private constructor(context: Context){
 
         private var INSTANCE: FornadaRepository? = null
     }
-//TODO Terminar 2:23
-//    fun get(): FornadaEntity? {
-//        var fornadaEntity : FornadaEntity? = null
-//        try {
-//            val cursor: Cursor
-//            val db = mFPadariaDatabaseHelper.readableDatabase
-//
-//            val projection = arrayOf(DataBaseConstants.FORNADA.COLUMNS.ID,
-//                    DataBaseConstants.FORNADA.COLUMNS.DESCRIPTION,
-//                    DataBaseConstants.FORNADA.COLUMNS.DATEFINISH)
-//
-//            val selection = "${DataBaseConstants.USER.COLUMNS.EMAIL} = ? AND ${DataBaseConstants.USER.COLUMNS.PASSWORD} = ?"
-//            //val selectionArgs = arrayOf(email, password)
-//            cursor = db.query(DataBaseConstants.USER.TABLE_NAME, projection, selection, selectionArgs, null, null, null)
-//            if(cursor.count > 0){
-//                cursor.moveToFirst()
-//                val userId = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.USER.COLUMNS.ID))
-//                val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.USER.COLUMNS.NAME))
-//                val email = cursor.getString(cursor.getColumnIndex(DataBaseConstants.USER.COLUMNS.EMAIL))
-//
-//                //Preencher entidade de user
-//                fornadaEntity = FornadaEntity(userId, name, email)
-//            }
-//            cursor.close()
-//        } catch (e: Exception) {
-//            return fornadaEntity
-//        }
-//        return fornadaEntity
-//    }
 
-    fun getList(): MutableList<FornadaEntity>{
+    fun get(): FornadaEntity? {
+        var fornadaEntity: FornadaEntity? = null
+        try {
+            val cursor: Cursor
+            val db = mFPadariaDatabaseHelper.readableDatabase
+
+            val projection = arrayOf(DataBaseConstants.FORNADA.COLUMNS.ID,
+                    DataBaseConstants.FORNADA.COLUMNS.DESCRIPTION,
+                    DataBaseConstants.FORNADA.COLUMNS.DATEFINISH)
+
+//            val selection = "${DataBaseConstants.FORNADA.COLUMNS.ID} = ? "
+//            val selectionArgs = arrayOf(id)
+            cursor = db.query(DataBaseConstants.FORNADA.TABLE_NAME, projection, null, null, null, null, null)
+            if (cursor.count > 0) {
+                cursor.moveToFirst()
+//                val userId = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.USER.COLUMNS.ID))
+                val fornadaId = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.FORNADA.COLUMNS.ID))
+                val description = cursor.getString(cursor.getColumnIndex(DataBaseConstants.FORNADA.COLUMNS.DESCRIPTION))
+                val dateFinish = cursor.getString(cursor.getColumnIndex(DataBaseConstants.FORNADA.COLUMNS.DATEFINISH))
+
+                //Preencher entidade de fornada
+                fornadaEntity = FornadaEntity(fornadaId, description, dateFinish)
+            }
+            cursor.close()
+        } catch (e: Exception) {
+            return fornadaEntity
+        }
+        return fornadaEntity
+    }
+
+    fun getList(): MutableList<FornadaEntity> {
         val list = mutableListOf<FornadaEntity>()
-        try{
-            val cursor : Cursor
+        try {
+            val cursor: Cursor
             val db = mFPadariaDatabaseHelper.readableDatabase
 
             cursor = db.rawQuery("SELECT* FROM ${DataBaseConstants.FORNADA.TABLE_NAME}", null)
-            if(cursor.count>0){
-                while (cursor.moveToNext()){
+            if (cursor.count > 0) {
+                while (cursor.moveToNext()) {
                     val id = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.FORNADA.COLUMNS.ID))
                     val description = cursor.getString(cursor.getColumnIndex(DataBaseConstants.FORNADA.COLUMNS.DESCRIPTION))
                     val dateFinish = cursor.getString(cursor.getColumnIndex(DataBaseConstants.FORNADA.COLUMNS.DATEFINISH))
@@ -66,9 +67,20 @@ class FornadaRepository private constructor(context: Context){
 
                 }
             }
-        }catch(e: Exception){
+        } catch (e: Exception) {
             return list
         }
         return list
+    }
+
+    //    fun insert(description: String, dateFinish: String): Int {
+    fun insert(fornada: FornadaEntity) {
+        //TODO: inserir o padeiro
+        val db = mFPadariaDatabaseHelper.writableDatabase
+        val insertValues = ContentValues()
+        insertValues.put(DataBaseConstants.FORNADA.COLUMNS.DESCRIPTION, fornada.description)
+        insertValues.put(DataBaseConstants.FORNADA.COLUMNS.DATEFINISH, fornada.dateFinish)
+
+        db.insert(DataBaseConstants.FORNADA.TABLE_NAME, null, insertValues)
     }
 }
